cf25115b2c9e23154bf277bf5895b59f5ac5d811
LordOfDarknesss <peter.peev95@gmail.com>
2018-03-14 18:15:03 +0200

    Pecaka i kaka ti Tanq nqmat butane

---
+++ VisualHTML+JS/main.js
@@ -0,0 +1,36 @@
+function hideShow(){
+    document.getElementById("tennis").classList.add("hidden");
+    document.getElementById("football").classList.remove("hidden");
+    document.getElementById("butonka1").classList.remove("hidden");
+    document.getElementById("butonka2").classList.add("hidden");
+    document.getElementById("fcolumn").classList.remove("hidden");
+    document.getElementById("tcolumn").classList.add("hidden");
+    document.getElementById("penalty").classList.remove("hidden");
+    document.getElementById("butonkaDolu").classList.remove("hidden");
+    document.getElementById("butonka4").classList.add("hidden");
+    document.getElementById("butonka3").classList.remove("hidden");
+    document.getElementById("butonka6").classList.remove("hidden");
+    document.getElementById("butonka7").classList.remove("hidden");
+    document.getElementById("butonka8").classList.remove("hidden");
+    document.getElementById("butonka9").classList.remove("hidden");
+    document.getElementById("goalsTab").classList.remove("hidden");
+}
+
+function hideShow1(){
+    document.getElementById("football").classList.add("hidden");
+    document.getElementById("tennis").classList.remove("hidden");
+    document.getElementById("butonka2").classList.remove("hidden");
+    document.getElementById("butonka1").classList.add("hidden");
+    document.getElementById("tcolumn").classList.remove("hidden");
+    document.getElementById("fcolumn").classList.add("hidden");
+    document.getElementById("penalty").classList.add("hidden");
+    document.getElementById("butonkaDolu").classList.add("hidden");
+    document.getElementById("butonka3").classList.add("hidden");
+    document.getElementById("butonka4").classList.remove("hidden");
+    document.getElementById("butonka6").classList.add("hidden");
+    document.getElementById("butonka7").classList.add("hidden");
+    document.getElementById("butonka8").classList.add("hidden");
+    document.getElementById("butonka9").classList.add("hidden");
+    document.getElementById("goalsTab").classList.add("hidden");
+}
+
